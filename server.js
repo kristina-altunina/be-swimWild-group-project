16cc1a0d508de718ca5f49869489f4858158e6cc
jakejones2 <jakejones1998@hotmail.co.uk>
2023-09-13 15:42:39 +0100

    add auth to server

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+
+const admin = require("firebase-admin");
+const serviceAccount = require("swimwild-c2ca7-firebase-adminsdk-yneo9-a17048c8f7.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 const app = express();
+
+// const admin = require('../config/firebase-config');
+
+app.use(
+  decodeToken(req, res, next).then(() => {
+    const token = req.headers.authorization.split(" ")[1];
+    try {
+      const decodeValue = admin.auth().verifyIdToken(token);
+      if (decodeValue) {
+        console.log(decodeValue);
+        return next();
+      }
+      return res.json({ message: "Unauthorized" });
+    } catch (e) {
+      return res.json({ message: "Internal Error" });
+    }
+  })
+);
 
 app.use(cors());
 
