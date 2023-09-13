f873a68c9a6b28a0899ea858d67ca6f1419717c2
jakejones2 <jakejones1998@hotmail.co.uk>
2023-09-13 12:31:25 +0100

    no auth

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 const express = require("express");
-const { auth, requiresAuth } = require("express-openid-connect");
+// const { auth, requiresAuth } = require("express-openid-connect");
 const https = require("https");
 const fs = require("fs");
 const cors = require("cors");
 const app = express();
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: "a long, randomly-generated string stored in env",
-  baseURL: "https://localhost:3000",
-  clientID: "5C2EqQWoR4YjcorL94wNGeXc3xV2Pe59",
-  issuerBaseURL: "https://dev-uebk5d5vk8z36y86.us.auth0.com",
-};
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   secret: "a long, randomly-generated string stored in env",
+//   baseURL: "https://localhost:3000",
+//   clientID: "5C2EqQWoR4YjcorL94wNGeXc3xV2Pe59",
+//   issuerBaseURL: "https://dev-uebk5d5vk8z36y86.us.auth0.com",
+// };
 
 app.use(cors());
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+// app.use(auth(config));
 
-app.get("/profile", requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
+// app.get("/profile", requiresAuth(), (req, res) => {
+//   res.send(JSON.stringify(req.oidc.user));
+// });
 
 // req.isAuthenticated is provided from the auth router
+
 app.get("/", (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+  res.status(200).send("hello");
 });
 
 // app.get("/callback", (req, res) => {
@@ -35,7 +36,7 @@ app.get("/", (req, res) => {
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
 
 // https
