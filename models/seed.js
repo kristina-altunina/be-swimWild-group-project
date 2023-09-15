f46e72f1d581080039f8fd6f386b86d5d547a050
jakejones2 <jakejones1998@hotmail.co.uk>
2023-09-15 14:10:28 +0100

    add local db for testing

--- models/seed.js
+++ models/seed.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.DATABASE_LOCAL_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
