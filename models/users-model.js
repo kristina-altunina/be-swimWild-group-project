28c540cd71a29dac04288b5e7f990992d3f036d8
Laura Patterson <laura.patterson53@hotmail.co.uk>
2023-09-15 11:08:13 +0100

    mins and km added

--- models/users-model.js
+++ models/users-model.js
@@ -56,6 +56,12 @@ const userSchema = mongoose.Schema({
           type: String,
           enum: ["freezing", "cold", "average", "warm", "hot"],
         },
+        mins: {
+          type: Number
+        },
+        km: {
+          type: Number
+        },
         outOfDepth: {
           type: Boolean,
         },
