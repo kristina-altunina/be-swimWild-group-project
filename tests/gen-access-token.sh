cca6c5aa643fe89252ee4ed9473c4c8d863353b5
jakejones2 <jakejones1998@hotmail.co.uk>
2023-09-18 10:58:07 +0100

    add access token sh

---
+++ tests/gen-access-token.sh
@@ -0,0 +1,3 @@
+curl 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyABjPb5ApAILuDCTMwHUPBh78nnCLhZmMg' \
+-H 'Content-Type: application/json' \
+--data-binary '{"email":"test@outlook.com","password":"password","returnSecureToken":true}'
