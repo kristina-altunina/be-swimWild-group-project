f320f5379af241ca7cfe72065238ba51b22b9a9f
Laura Patterson <laura.patterson53@hotmail.co.uk>
2023-09-19 16:36:49 +0100

    more locations added

--- test-data/locations.js
+++ test-data/locations.js
@@ -23,4 +23,52 @@ module.exports = [
       coordinates: [56.188753, -2.830412],
     },
   },
+  {
+    name: "River Isis, Port Meadow, Oxford",
+    type: "river",
+    loc: {
+      type: "Point",
+      coordinates: [51.772329824620996, -1.2835620054092063],
+    },
+  },
+  {
+    name: "Linhope Spout, Northumberland",
+    type: "pond",
+    loc: {
+      type: "Point",
+      coordinates: [55.447944356182965, -2.067671030695456],
+    },
+  },
+  {
+    name: "Falls of Falloch, Crianlarich, Scotland",
+    type: "pond",
+    loc: {
+      type: "Point",
+      coordinates: [55.662038436359275, -3.7802584005746165],
+    },
+  },
+  {
+    name: "Goldiggins Quarry, Minions, Cornwall",
+    type: "lake",
+    loc: {
+      type: "Point",
+      coordinates: [50.5250108622856, -4.471285995316559],
+    },
+  },
+  {
+    name: "Walpole Bay Tidal Pool, Margate",
+    type: "sea",
+    loc: {
+      type: "Point",
+      coordinates: [51.388631888089755, 1.3796788791465624],
+    },
+  },
+  {
+    name: "Blue Lagoon, Pembrokeshire, Wales",
+    type: "sea",
+    loc: {
+      type: "Point",
+      coordinates: [51.93842606503727, -5.207754321139982],
+    },
+  },
 ];
