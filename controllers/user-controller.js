b7ec2d4b3d4e72d80dcb1224b46c64d4e786abc3
jakejones2 <jakejones1998@hotmail.co.uk>
2023-09-19 11:49:47 +0100

    refactor comments and package.json

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -19,19 +19,18 @@ function postUser(req, res, next) {
 
 function patchUser(req, res, next) {
   const { nickname, profileImg } = req.body;
-  const filter = {uid: req.user.uid}
-  const update = {nickname: nickname,
-  profileImg: profileImg}
+  const filter = { uid: req.user.uid };
+  const update = { nickname: nickname, profileImg: profileImg };
   Users.findOneAndUpdate(filter, update)
-  .then(()=>{
-    return Users.findOne(filter)
-  })
-  .then((newUser)=>{
-    res.status(200).send(newUser)
-  })
-  .catch((err)=>{
-    console.log("why here")
-  })
+    .then(() => {
+      return Users.findOne(filter);
+    })
+    .then((newUser) => {
+      res.status(200).send(newUser);
+    })
+    .catch((err) => {
+      console.log("why here");
+    });
 }
 
 function getUser(req, res, next) {
@@ -47,43 +46,3 @@ function getUser(req, res, next) {
 }
 
 module.exports = { postUser, getUser, patchUser };
-
-// app.get("/", (req, res) => {
-//   res.status(200).send({ greeting: `hello ${req.user.email}` });
-// });
-
-// app.get("/locations", (req, res) => {
-//   Locations.find({}).then((locations) => {
-//     res.status(200).json(locations);
-//   });
-// });
-
-// app.get("/locations/:id", (req, res) => {
-//   const { id } = req.params;
-
-//   Locations.find({ id: id })
-//     .then((location) => {
-//       res.status(200).json(location);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get("/users", (req, res) => {
-//   Users.find({}).then((users) => {
-//     res.status(200).json(users);
-//   });
-// });
-
-// app.get("/profile", (req, res) => {
-//   Users.find({ uid: req.user.uid }).then((user) => {
-//     res.status(200).json(user);
-//   });
-// });
-
-// app.post("/locations", (req, res) => {
-//   Locations.create(req.body).then((location) => {
-//     res.status(200).json(location);
-//   });
-// });
