c7625d73ac2c56f4c18e9c74c878d8a926c980df
jakejones2 <jakejones1998@hotmail.co.uk>
2023-09-26 16:56:04 +0100

    remove forEach in validate PostBody

--- middleware/validators/user-validators.js
+++ middleware/validators/user-validators.js
@@ -1,13 +1,12 @@
 const isURL = require("is-url");
 
-
 function validateUserPatchBody(req, res, next) {
-  const patchBodyKeys = Object.keys(req.body)
-  patchBodyKeys.forEach((key)=>{
-    if(!["nickname", "profileImg"].includes(key)){
-       return res.status(400).send({msg: "invalid patch body"})
+  const patchBodyKeys = Object.keys(req.body);
+  patchBodyKeys.forEach((key) => {
+    if (!["nickname", "profileImg"].includes(key)) {
+      return res.status(400).send({ msg: "invalid patch body" });
     }
-  })
+  });
   const { nickname, profileImg } = req.body;
   if (!nickname && !profileImg) {
     return res.status(400).send({
@@ -23,33 +22,35 @@ function validateUserPatchBody(req, res, next) {
   next();
 }
 
-function validatePostBody(req,res,next){
-  const{name, nickname, dob, profileImg, bio, home} = req.body
-  const postBodyKeys = Object.keys(req.body)
-  postBodyKeys.forEach((key)=>{
-    if(!["name", "nickname", "profileImg", "dob", "bio", "home"].includes(key)){
-       return res.status(400).send({msg: "invalid post body"})
+function validatePostBody(req, res, next) {
+  const { name, nickname, dob, profileImg, bio, home } = req.body;
+  const postBodyKeys = Object.keys(req.body);
+  for (const key of postBodyKeys) {
+    if (
+      !["name", "nickname", "profileImg", "dob", "bio", "home"].includes(key)
+    ) {
+      return res.status(400).send({ msg: "One of the given keys is invalid" });
     }
-  })
-  if (nickname && typeof nickname !== "string") {
-    return res.status(400).send({ msg: "Nickname should be a string" });
+    if (nickname && typeof nickname !== "string") {
+      return res.status(400).send({ msg: "Nickname should be a string" });
+    }
+    if (name && typeof name !== "string") {
+      return res.status(400).send({ msg: "Nickname should be a string" });
+    }
+    if (profileImg && !isURL(profileImg)) {
+      return res.status(400).send({ msg: `${profileImg} is not a valid URL` });
+    }
+    if (bio && typeof bio !== "string") {
+      return res.status(400).send({ msg: "bio should be a string" });
+    }
+    if (home && typeof home !== "string") {
+      return res.status(400).send({ msg: "bio should be a string" });
+    }
+    if (dob && typeof dob !== "string") {
+      return res.status(400).send({ msg: "dob should be a string" });
+    }
+    return next();
   }
-  if (name && typeof name !== "string") {
-    return res.status(400).send({ msg: "Nickname should be a string" });
-  }
-  if (profileImg && !isURL(profileImg)) {
-    return res.status(400).send({ msg: `${profileImg} is not a valid URL` });
-  }
-  if (bio && typeof bio !== "string") {
-    return res.status(400).send({ msg: "bio should be a string" });
-  }
-  if (home && typeof home !== "string") {
-    return res.status(400).send({ msg: "bio should be a string" });
-  }
-  if (dob && typeof dob !== "string") {
-    return res.status(400).send({ msg: "dob should be a string" });
-  }
-  next()
 }
 
 module.exports = { validateUserPatchBody, validatePostBody };
