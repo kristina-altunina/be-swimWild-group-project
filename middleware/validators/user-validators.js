db1a15e81722b0e6695d345f9cd0ad9720a70401
jakejones2 <jakejones1998@hotmail.co.uk>
2023-09-26 17:34:20 +0100

    remove superfluous keys validator

--- middleware/validators/user-validators.js
+++ middleware/validators/user-validators.js
@@ -26,33 +26,26 @@ function validatePostBody(req, res, next) {
   console.log("in the validator");
   const { name, nickname, dob, profileImg, bio, home } = req.body;
   const postBodyKeys = Object.keys(req.body);
-  for (const key of postBodyKeys) {
-    if (
-      !["name", "nickname", "profileImg", "dob", "bio", "home"].includes(key)
-    ) {
-      return res.status(400).send({ msg: "One of the given keys is invalid" });
-    }
-    if (nickname && typeof nickname !== "string") {
-      return res.status(400).send({ msg: "Nickname should be a string" });
-    }
-    if (name && typeof name !== "string") {
-      return res.status(400).send({ msg: "Nickname should be a string" });
-    }
-    if (profileImg && !isURL(profileImg)) {
-      return res.status(400).send({ msg: `${profileImg} is not a valid URL` });
-    }
-    if (bio && typeof bio !== "string") {
-      return res.status(400).send({ msg: "bio should be a string" });
-    }
-    if (home && typeof home !== "string") {
-      return res.status(400).send({ msg: "bio should be a string" });
-    }
-    if (dob && typeof dob !== "string") {
-      return res.status(400).send({ msg: "dob should be a string" });
-    }
-    console.log("passed validator");
-    return next();
+  if (nickname && typeof nickname !== "string") {
+    return res.status(400).send({ msg: "Nickname should be a string" });
   }
+  if (name && typeof name !== "string") {
+    return res.status(400).send({ msg: "Nickname should be a string" });
+  }
+  if (profileImg && !isURL(profileImg)) {
+    return res.status(400).send({ msg: `${profileImg} is not a valid URL` });
+  }
+  if (bio && typeof bio !== "string") {
+    return res.status(400).send({ msg: "bio should be a string" });
+  }
+  if (home && typeof home !== "string") {
+    return res.status(400).send({ msg: "bio should be a string" });
+  }
+  if (dob && typeof dob !== "string") {
+    return res.status(400).send({ msg: "dob should be a string" });
+  }
+  console.log("passed validator");
+  return next();
 }
 
 module.exports = { validateUserPatchBody, validatePostBody };
