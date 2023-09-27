e6b8b2e94312fdb7e7b44830f7aa5734d653d247
Kristina-Altunina <kristina@Kristinas-MBP.broadband>
2023-09-27 10:46:26 +0100

    message fixed if nickname is in use

--- middleware/errorHandlers.js
+++ middleware/errorHandlers.js
@@ -1,6 +1,6 @@
 function mongoDbErrors(err, req, res, next) {
   if (err?.code === 11000) {
-    return res.status(400).send(`${err.keyValue} is already taken!`);
+    return res.status(400).send({ errorCode: 'duplicate-nickname', msg: "nickname already in use" });
   }
   if (err._message) {
     return res.status(400).send(err._message);
